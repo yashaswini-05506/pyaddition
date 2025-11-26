f200b7135c8a16a73a3f87439c71ab29666568af
yashaswini-05506 <yashaswinimadnallimath@gmail.com>
2025-11-26 11:02:13 +0530

    Update test_addition.py

--- test_addition.py
+++ test_addition.py
@@ -1,10 +1,10 @@
 from addtion import add
 
-def test_add_postitive_numbers():
+def test_add_positive_numbers():
     assert add(2, 3) ==5
 
-    def test_add_negative_number():
-        assert add(4, 6) == 10
+    def test_add_negative_numbers():
+        assert add(-4, -6) == -10
 
         def test_add_zero():
             assert add(0,5) == 5
